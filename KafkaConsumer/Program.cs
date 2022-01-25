67a5baa4acdd777455d2e491233a4ef09e087b9f
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2022-01-25 13:17:14 +0100

    Add project files.

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "172.19.95.154:9092",
+    GroupId = "Gruppe",
+    AutoOffsetReset = AutoOffsetReset.Latest,
+};
+
+using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    consumer.Subscribe("quickstart-events");
+
+    Console.WriteLine("Klar til at modtage beskeder");
+
+    while (true)
+    {
+        var consumeResult = consumer.Consume();
+        string message = consumeResult.Message.Value;
+
+        Console.WriteLine($"Besked modtaget: {message}");
+    }
+    consumer.Close();
+}
